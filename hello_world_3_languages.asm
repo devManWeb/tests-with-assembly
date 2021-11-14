7e97e4e7d3e712cc874d5bfec70493225b96654f
Leonardo Bissaro <43416629+devManWeb@users.noreply.github.com>
2021-11-14 15:01:45 +0100

    Update hello_world_3_languages.asm

--- hello_world_3_languages.asm
+++ hello_world_3_languages.asm
@@ -3,36 +3,36 @@
 
 section .data
 	textEng db "Hello, World!", 10
-    textIta db "Ciao, Mondo!", 10
-    textDeu db "Hallo, Welt!", 10
+	textIta db "Ciao, Mondo!", 10
+	textDeu db "Hallo, Welt!", 10
 
 section .text
 	global _start
 
 _start:
 
-    ; print the english phrase
+	; print the english phrase
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, textEng
 	mov rdx, 14         ; string length
 	syscall
 
-    ; print the italian phrase
+	; print the italian phrase
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, textIta
 	mov rdx, 13         ; string length
 	syscall
 
-    ; print the german phrase
+	; print the german phrase
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, textDeu
 	mov rdx, 13         ; string length
 	syscall
 
-    ; program termination
+	; program termination
 	mov rax,60
 	mov rdi,0
 	syscall
